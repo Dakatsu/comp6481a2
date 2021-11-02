f5eb6f62f71ff706d1fb997383bee31486c799c2
Kyle Taylor Lange <dakatsu@gmail.com>
2021-11-01 21:22:57 -0400

    Made output files opened once per file instead of once per article.

--- BibCreator.java
+++ BibCreator.java
@@ -8,43 +8,48 @@ import java.io.FileInputStream;
 public class BibCreator {
 
 	static String author,journal,title,year,volume,number,pages,doi,ISSN,month;
-	static int articleCount = 1, fileCount = 1;
+	static int fileCount = 1;
 	
 	public static void processFilesForValidation(Scanner[] inputFiles) {
+		// Process each file one at a time.
 		for (int i = 0; i < inputFiles.length; i++) {
 			Scanner sc = inputFiles[i];
-			articleCount = 1;
+			PrintWriter pw1 = null, pw2 = null, pw3 = null;
+			try
+			{
+				pw1 = new PrintWriter(new FileOutputStream("IEEE" + fileCount + ".txt"));
+				pw2 = new PrintWriter(new FileOutputStream("ACM" + fileCount + ".txt"));
+				pw3 = new PrintWriter(new FileOutputStream("NJ" + fileCount + ".txt"));
+			}
+			catch(FileNotFoundException e) 			// Since we are attempting to write to the file
+			{							   	// exception is automatically thrown if file cannot be created. 
+				System.out.println("Could not create a file to write to. "
+						+ " Please check for problems such as directory permission"
+						+ " or no available memory.");	
+				System.out.print("Program will terminate.");
+				System.exit(0);			   		   
+			}
+			int articleCount = 1;
+			// Read the input file.
 			while(sc.hasNextLine()) {
 				String s = sc.nextLine();
+				// If we come across an article, read its contents and add it to the file if it's valid.
 				if(s.startsWith("@ARTICLE{")) {
-					boolean bShouldWriteToFile = true;
+					boolean bWriteArticleToFile = true;
 					while (!s.equals("}")) {
 						s = sc.nextLine();
 						try {
 							format(s);
 						}
+						// Stop reading this article if it is invalid.
 						catch (FileInvalidException e) {
-							System.out.println("Exception when reading file " + fileCount +  ": " + e.getMessage());
-							bShouldWriteToFile = false;
+							System.out.println("Exception when reading file " + fileCount + "." + articleCount + ": " + e.getMessage());
+							bWriteArticleToFile = false;
+							break;
 						}
 					}
-					if (bShouldWriteToFile) {
+					if (bWriteArticleToFile) {
 						System.out.println("File " + fileCount + "." + articleCount);
-						PrintWriter pw1 = null, pw2 = null, pw3 = null;	
-						try
-						{
-							pw1 = new PrintWriter(new FileOutputStream("IEEE" + fileCount + ".txt", true));
-							pw2 = new PrintWriter(new FileOutputStream("ACM" + fileCount + ".txt", true));
-							pw3 = new PrintWriter(new FileOutputStream("NJ" + fileCount + ".txt", true));
-						}
-						catch(FileNotFoundException e) 			// Since we are attempting to write to the file
-						{							   	// exception is automatically thrown if file cannot be created. 
-							System.out.println("Could not create a file to write to. "
-									+ " Please check for problems such as directory permission"
-									+ " or no available memory.");	
-							System.out.print("Program will terminate.");
-							System.exit(0);			   		   
-						}
 				
 						String author1 = author.replaceAll(" and", ",");
 						int andIndex = author.indexOf("and");
@@ -52,19 +57,21 @@ public class BibCreator {
 						String author3 = author.replaceAll("and", "&");
 				
 				        pw1.println(author1+". \""+title+"\", "+journal+", vol. "+volume+", no. "+number+", p. "+pages+", "+month+" "+year+".");
-						pw1.close();
 						pw2.println("[" + articleCount + "] "+author2+" "+title+". "+journal+". "+volume+", "+number+" (+"+year+"), "+pages+". DOI:https://doi.org/"+doi+".");
-						pw2.close();
 						pw3.println(author3+". "+title+". "+journal+". "+volume+", "+pages+"("+year+").");
-						pw3.close();
 						articleCount++;
 					}
+					// Stop reading this file if there is an invalid article.
 					else {
 						break;
 					}
 				}	
 			}
 			fileCount++;
+			// Close the output writers.
+			pw1.close();
+			pw2.close();
+			pw3.close();
 			// Close file after reading.
 			inputFiles[i].close();
 		}
